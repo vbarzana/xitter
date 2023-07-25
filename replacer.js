8cdfc2fc210527049799479afbb6f1979e44331b
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2023-07-25 10:12:01 +0300

    feat: providing first throw of a chrome extension to replace twitter with xitter and tweet by xeet in twitter's website.

---
+++ replacer.js
@@ -0,0 +1,61 @@
+const TWITTER_REGEXP = /Twitter/gi
+const TWEET_REGEXP = /Tweet/gi
+
+const replaceText = (node) => {
+    if (node.nodeValue.match(TWEET_REGEXP) || node.nodeValue.match(TWEET_REGEXP)) {
+        node.nodeValue = node.nodeValue
+            .replace(TWITTER_REGEXP, 'Xitter')
+            .replace(TWEET_REGEXP, 'Xeet');
+    }
+};
+
+const walk = (node) => {
+    if (node && node.getAttribute && node.getAttribute('data-testid') === 'tweetButtonInline') {
+        console.log('found tweet button', node);
+    }
+    if (!node || !node.getAttribute || node.getAttribute('data-testid') === 'tweetText') {
+        return;
+    }
+    const childNodes = node.childNodes;
+    for (let i = 0; i < childNodes.length; i++) {
+        const childNode = childNodes[i];
+        if (childNode.nodeType === Node.TEXT_NODE) {
+            replaceText(childNode);
+        } else {
+            walk(childNode);
+        }
+    }
+};
+
+function updateTweetButton() {
+    const tweetButton = document.querySelector('[data-testid="tweetButtonInline"]');
+    walk(tweetButton);
+}
+
+function observeDOMChanges() {
+    const observer = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                for (const node of mutation.addedNodes) {
+                    if (node.nodeType === Node.TEXT_NODE) {
+                        replaceText(node);
+                    } else {
+                        walk(node);
+                    }
+                }
+            } else if (mutation.type === 'characterData') {
+                walk(mutation.target);
+            }
+        }
+    });
+
+    observer.observe(document.body, {subtree: true, characterData: true, childList: true});
+}
+
+updateTweetButton();
+walk(document.body);
+
+setTimeout(function () {
+    updateTweetButton();
+    observeDOMChanges();
+}, 1000);
